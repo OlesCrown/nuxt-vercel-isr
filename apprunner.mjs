4552cc1e3589cc20bc911d4f033f0f989313bab6
Oles Crown <25193332+OlesCrown@users.noreply.github.com>
2023-12-17 23:03:28 -0500

    a

--- apprunner.mjs
+++
@@ -1,4 +0,0 @@
-import { createServer } from "node:http";
-import { listener } from "./.output/server/index.mjs";
-const server = createServer(listener);
-server.listen("/tmp/nginx.socket"); //following the buildpack doc
--- initServer.ts
+++
@@ -1,4 +0,0 @@
-import fs from "fs"
-export default defineNitroPlugin((nitroApp) => {
-   if((process.env.NODE_ENV || 'development') != 'development')fs.openSync('/tmp/app-initialized', 'w')
-})
